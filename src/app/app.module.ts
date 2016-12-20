2a3650da8c73800ecff0b0955cfe935dce5687db
Nil Panés <nil.panes@gmail.com>
2016-12-20 22:30:07 +0100

    Added firebase support and improved look of the home screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,20 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MnFullpageDirective, MnFullpageService } from "ng2-fullpage";
+import { AngularFireModule } from 'angularfire2';
+
 
 import { AppComponent } from './app.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ProjectDetailComponent } from './project-detail/project-detail.component';
 import { MasterComponent } from './master/master.component';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDghV4zUgHAK6fg7waocCDyLy7PzAYhJMU',
+  authDomain: 'adriana-portfolio.firebaseapp.com',
+  databaseURL: 'https://adriana-portfolio.firebaseio.com',
+  storageBucket: 'adriana-portfolio.appspot.com'
+};
 
 @NgModule({
   declarations: [
@@ -20,7 +29,8 @@ import { MasterComponent } from './master/master.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [
     MnFullpageService
--- src/app/master/master.component.ts
+++ src/app/master/master.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-master',
   templateUrl: './master.component.html',
   styleUrls: ['./master.component.css']
 })
-export class MasterComponent implements OnInit {
+export class MasterComponent {
+  items: FirebaseListObservable<any[]>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(af: AngularFire) {
+    this.items = af.database.list('/projects');
   }
-
 }
