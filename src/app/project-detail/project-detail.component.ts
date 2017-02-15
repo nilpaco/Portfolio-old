781f36a2b31846bc727e8bccd4f7b4c19847d0f3
Nil Panés Coll <nil.panes@gmail.com>
2017-02-15 14:46:21 +0100

    Fullpage session destroyed

--- src/app/project-detail/project-detail.component.ts
+++ src/app/project-detail/project-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { MnFullpageService } from 'ng2-fullpage';
 
 @Component({
   selector: 'app-project-detail',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-detail.component.css']
 })
 export class ProjectDetailComponent implements OnInit {
+  constructor(private location: Location, private fullpageService: MnFullpageService) {
+   }
 
-  constructor() { }
+  goBack(): void {
+      this.location.back();
+  }
 
   ngOnInit() {
+    this.fullpageService.destroy('all');
   }
 
 }
